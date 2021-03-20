5d714645be643c519d25223a5d0d541619cd9a51
velykitty <jiyunee13@gmail.com>
2021-03-20 17:14:38 +0900

    build news viewer ui -make a draft of NewsList.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5f75d61e791946fcac793b56a6e3c807',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-    // axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-    //   setData(response.data);
-    // });
-  };
-
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
+    <NewsList />
   );
 }
 
