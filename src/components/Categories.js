4db8da497329878d722aecb85ec8cfc218d0ec09
velykitty <jiyunee13@gmail.com>
2021-03-21 02:09:52 +0900

    선택된 카테고리 폰트 컬러 변경

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const categories = [
     {
@@ -55,16 +55,28 @@ const Category = styled.div`
         color: #495057;
     }
 
+    ${props => 
+    props.active && css`
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover {
+            color: #3bc9db;
+        }
+    `}
+
     & + & {
         margin-left: 1rem;
     }
 `;
 
-const Categories = () => {
+const Categories = ({onSelect, category}) => {
     return (
         <CategoriesBlock>
-            {categories.map(category => (
-                <Category Key={category.name}>{category.text}</Category>
+            {categories.map(c => (
+                <Category Key={c.name}
+                active={category === c.name}
+                onClick={() => onSelect(c.name)}>{c.text}</Category>
             ))}
         </CategoriesBlock>
     );
