1afb03eb6cec41bc8bb6cfaec1751cab04d7d9ca
velykitty <jiyunee13@gmail.com>
2021-03-21 13:00:58 +0900

    react router -apply navlink to categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
+import {NavLink} from 'react-router-dom';
 
 const categories = [
     {
@@ -43,7 +44,7 @@ const CategoriesBlock = styled.div`
     }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
     font-size: 1.125rem;
     cursor: pointer;
     white-space: pre;
@@ -65,18 +66,29 @@ const Category = styled.div`
         }
     `}
 
+    &.active {
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover {
+            color: #3bc9db;
+        }
+    }
+
     & + & {
         margin-left: 1rem;
     }
 `;
 
-const Categories = ({onSelect, category}) => {
+const Categories = () => {
     return (
         <CategoriesBlock>
             {categories.map(c => (
                 <Category Key={c.name}
-                active={category === c.name}
-                onClick={() => onSelect(c.name)}>{c.text}</Category>
+                activeClassName="active"
+                exact={c.name === 'all'}
+                to={c.name === 'all' ? '/' : `/${c.name}`}
+                >{c.text}</Category>
             ))}
         </CategoriesBlock>
     );
